1304fd23619c0e9b6e28863c3ccb4dd540de54e8
SPK80 <psukonkin@gmail.com>
2022-06-04 20:25:58 +0300

    fix: onChangeHandler in InputNumber

--- src/components/InputNumber.tsx
+++ src/components/InputNumber.tsx
@@ -12,15 +12,18 @@ type InputType = {
 }
 
 export const InputNumber: React.FC<InputType> = (props) => {
+    
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        const val = +e.currentTarget.value;
-        if (Number.isNaN(val)) return
-        props.onChangeValue(val)
+        const val = e.currentTarget.valueAsNumber
+        e.currentTarget.value = ''
+        if (Number.isNaN(val))
+            props.onChangeValue(0)
+        else
+            props.onChangeValue(val)
     }
     
     const finalCaptionClassName = `${s.caption} ${props.captionClassName ?? ''}`
     const finalInputClassName = `${s.input} ${props.inputClassName ?? ''} ${props.error ? s.errorInput : ''} `
-    
     return (
         <div className={s.captionAndInputWrap}>
             {props.caption && <span className={finalCaptionClassName}>{props.caption}</span>}
